e7887134f17cfcea5d8aa1e0bd07716205bb5b5c
maalkila <majdkilany92@gmail.com>
2026-06-22 18:31:42 +0300

    add new

---
+++ c03/ex04/ft_strstr.c
@@ -0,0 +1,19 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (to_find[0] == '\0')
+		return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
+	}
+	return (0);
+}
