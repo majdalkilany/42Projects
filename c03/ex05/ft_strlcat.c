115ca6a170bef9ecc5deec309d7773be05fdf31c
maalkila <majdkilany92@gmail.com>
2026-06-22 18:39:20 +0300

    add new

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,23 @@
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dlen;
+	unsigned int	slen;
+	unsigned int	i;
+
+	dlen = 0;
+	while (dlen < size && dest[dlen] != '\0')
+		dlen++;
+	slen = 0;
+	while (src[slen] != '\0')
+		slen++;
+	if (dlen == size)
+		return (size + slen);
+	i = 0;
+	while (src[i] != '\0' && dlen + i < size - 1)
+	{
+		dest[dlen + i] = src[i];
+		i++;
+	}
+	dest[dlen + i] = '\0';
+	return (dlen + slen);
+}
